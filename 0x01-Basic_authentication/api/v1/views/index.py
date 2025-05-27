7499bd6bc0ee7836d96998d164a26062be6edb38
makau6 <makauwanyoike@gmail.com>
2025-05-27 19:45:16 +0300

    forbidden routes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -23,6 +23,15 @@ def unauthorized() -> str:
     abort(401, description="Unauthorized request")
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - a 403 error with a message
+    """
+    abort(403, description="Forbidden access")
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
