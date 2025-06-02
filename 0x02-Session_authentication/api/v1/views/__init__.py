0600d1fdd7f5acc6ec3dbfa3dfa0fc4ec29de1c9
makau6 <makauwanyoike@gmail.com>
2025-06-02 21:52:55 +0300

    rearrange

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -2,14 +2,15 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.app import *
 from models.user import *
 from api.v1.views.auth import *
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.auth.session_auth import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
 User.load_from_file()
