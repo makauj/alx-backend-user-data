d1beb30c5b77a85ef3347d7757574999aa3d1f6f
makau6 <makauwanyoike@gmail.com>
2025-06-06 18:38:21 +0300

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -125,16 +125,17 @@ def update_password() -> str:
         - 403 if not valid reset token
         - 200 and JSON Payload if valid
     """
-    try:
-        email = request.form['email']
-        reset_token = request.form['reset_token']
-        new_password = request.form['new_password']
-    except KeyError:
-        abort(400)
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    is_password_changed = False
 
     try:
         AUTH.update_password(reset_token, new_password)
+        passwd_changed = True
     except ValueError:
+        passwd_changed = False
+    if not passwd_changed:
         abort(403)
 
     msg = {"email": email, "message": "Password updated"}
