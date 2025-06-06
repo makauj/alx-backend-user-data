d85a25f6642cca0f409e5fd0418561b6bdb4115f
makau6 <makauwanyoike@gmail.com>
2025-06-06 13:42:53 +0300

    sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Users SQLAlchemy model."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+class User():
+    """User class for SQLAlchemy ORM."""
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(128), nullable=False, unique=True)
+    hashed_password = Column(String(128), nullable=False)
+    session_id = Column(String(128), nullable=True)
+    reset_token = Column(String(128), nullable=True)
