c2d2bb2ba293bc6d61596babf1972f73f0278a4e
makau6 <makauwanyoike@gmail.com>
2025-06-06 14:08:54 +0300

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,7 @@ Base = declarative_base()
 
 class User(Base):
     """User class for SQLAlchemy ORM."""
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
