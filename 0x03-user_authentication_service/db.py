a7df4351ac2c0d79df66a02d9222559c77fbc46b
makau6 <makauwanyoike@gmail.com>
2025-06-06 16:45:22 +0300

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,7 @@
 """
 python script to implent the add_user method
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
@@ -41,3 +41,41 @@ class DB:
         self._session.commit()
         self._session.refresh(user)
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ 
+        method takes in arbitrary keyword arguments and returns the first
+        row found in the users table as filtered by the method’s
+        input arguments.
+        No validation of input arguments required at this point
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except Exception:
+            raise InvalidRequestError()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError()
+        self._session.commit()
